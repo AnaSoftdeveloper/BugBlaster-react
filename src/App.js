359707ae768d9c274416ca8a881ee19f542ac580
Anapa <Anapa@ANAHITA>
2024-07-18 12:07:05 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-       <h1>Bug Blaster</h1>
+       <h1>Bug Blaster V2</h1>
        <TicketForm></TicketForm>
       </div>
     </div>
